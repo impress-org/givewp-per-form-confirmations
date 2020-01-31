bb186ba465cf454f7172daa8e715ac4dc0d5fd10
Matt Cromwell <reachme@mattcromwell.com>
2020-01-31 14:54:00 -0800

    Fixes #1 by making the redirect logic work as intended.

--- includes/main-functions.php
+++ includes/main-functions.php
@@ -7,13 +7,13 @@ function pfconfs_4_give_redirects( $success_page ) {
     $success_page = isset( $give_options['success_page'] ) ? get_permalink( absint( $give_options['success_page'] ) ) : get_bloginfo( 'url' );
 
     $form_id = isset( $_POST['give-form-id'] ) ? $_POST['give-form-id'] : 0;
-    $form_meta = get_post_meta( $form_id );
+
     $redirect_status = get_post_meta( $form_id, 'pfconfs4givewp-fields_status', true);
     $conf_url = get_post_meta( $form_id, 'pfconfs4givewp-fields_page_url', true);
 
-	if ( $redirect_status = 'customize' ) {
-		$success_page = esc_url( $conf_url );
-	} 
+	if ( $redirect_status == 'customize' ) {
+		$success_page = $conf_url[0];
+    } 
 
 	return $success_page;
 
